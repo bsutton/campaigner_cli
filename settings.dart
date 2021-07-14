f3e44bddca827429ae86315c0ee7f4a305b1f8bc
Brett Sutton <bsutton@noojee.com.au>
2021-07-14 14:43:53 +1000

    initial commit.

---
+++ settings.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:dcli/dcli.dart';
+import 'package:settings_yaml/settings_yaml.dart';
+
+class Settings {
+  late final String url;
+  late final String apiKey;
+
+  late final String smsApiKey;
+  late final String smsSecret;
+
+  late final String defaultMobile;
+  Settings.load() {
+    var file = '.settings.yaml';
+    var yaml = SettingsYaml.load(pathToSettings: file);
+    url = fetch(yaml, 'url');
+    apiKey = fetch(yaml, 'apiKey');
+    smsApiKey = fetch(yaml, 'smsApiKey');
+    smsSecret = fetch(yaml, 'smsSecret');
+    defaultMobile = fetch(yaml, 'defaultMobile');
+  }
+
+  String fetch(SettingsYaml yaml, String name) {
+    var value = yaml[name] as String?;
+    if (value == null) {
+      printerr('Your .settings.yaml is missing a value for $name');
+      exit(1);
+    }
+    return value;
+  }
+}
